e78cdde27fb1152009d3d5ad7f2bec604cb90460
BABY\SEONGEUN <yooseongeun@naver.com>
2022-11-22 15:15:37 +0900

    main작업 끝, login, signup은 깃 병합 후에 backend와 같이 작업할것

--- paintravel-frontend/src/components/post/ContentList.js
+++ paintravel-frontend/src/components/post/ContentList.js
@@ -1,8 +1,7 @@
 import '../../styles/post/contentList.scss';
+import {Link} from 'react-router-dom';
 
 function ContentList({selectedCountry}) {
-  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
-
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
     // eslint-disable-next-line react/jsx-filename-extension
