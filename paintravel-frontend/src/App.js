5b560e53fe8d789bac4edc5143528dcba84dfca7
BABY\SEONGEUN <yooseongeun@naver.com>
2022-11-22 19:31:06 +0900

    postWrite 프론트엔드 일단 끝

--- paintravel-frontend/src/App.js
+++ paintravel-frontend/src/App.js
@@ -1,9 +1,7 @@
 /* eslint-disable */
 import { Routes, Route } from 'react-router';
 import MyPagePage from './pages/MyPagePage';
-import PostPage from './pages/PostPage';
 import Login from './components/Login/Login';
-import None from './pages/None';
 import NotFound from './NotFound';
 import MapPage from './pages/MapPage';
 import PostWritePage from './pages/PostWritePage';
@@ -15,10 +13,8 @@ function App() {
       <Route path="/" element={<MapPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/mypage" element={<MyPagePage />} />
-      <Route path="/post" element={<PostPage />} />
       <Route path="/postwrite" element={<PostWritePage />} />
       <Route path="/*" element={<NotFound />} />
-      <Route path="/none" element={<None />} />
     </Routes>
   );
 }
--- paintravel-frontend/src/components/post/Post.js
+++ paintravel-frontend/src/components/post/Post.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React , {useState, } from "react";
 import "../../styles/post/postWrite.scss";
+import {useNavigate} from 'react-router-dom';
 
 function Post() {
+  const [post, setPost] = useState({
+    location:'',
+    fromDate:'',
+    toDate:'',
+    content:'',
+    files:'',
+  });
+  const [uploadImages, setUploadImages] = useState([]);
+  const navigate = useNavigate();
+
+  const onChangePost = (e) => {
+    setPost({
+      ...post,
+      [e.target.name]:e.target.value,
+    })
+  }
+  const onLoadFile = (e) => {
+    const files = e.target.files;
+    if(files.length>4){
+      e.preventDefault();
+      alert("이미지 개수는 4개를 넘을 수 없습니다!");
+      return;
+    }
+    let imageUrlLists = [...post.files];
+
+    for (let i = 0; i < files.length; i++) {
+      const currentImageUrl = URL.createObjectURL(files[i]);
+      imageUrlLists.push(currentImageUrl);
+    }
+
+    setPost({
+      ...post,
+      [post.files]:imageUrlLists
+    })
+    setUploadImages(imageUrlLists);
+  }
+
+  const deleteImage = (id) => {
+    setPost({
+      ...post,
+      [post.files]:post.files.filter((index) => index !== id)
+    })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(post);
+  }
+
+  const goMain = () => {
+    navigate('/');
+  }
   return (
   <div className="postWriteContainer">
     <div className="postWrite">
@@ -10,30 +63,35 @@ function Post() {
         <div className="gallery">
           <h2>Gallery</h2>
           <label htmlFor="galleryUpload">+</label>
-          <input type="file" id="galleryUpload"></input>
+          <input type="file" multiple={true} id="galleryUpload" onChange={onLoadFile} accept="image/jpg,image/png,image/jpeg,image/gif"/>
           <div className="galleryContainer">
-
+            {uploadImages.map((image, id) => (
+              <div key={id} style={{width:'100px', height:'75px', display:'block'}}>
+                <img src={image} alt={`${image} - ${id}`}/>
+                <span onClick={deleteImage} >X</span>
+              </div>
+            ))}
           </div>
         </div>
         <ul>
           <li>
             <p>위치</p>
-            <input type="text"></input>
+            <input type="text" name='location' onChange={onChangePost}></input>
           </li>
           <li>
             <p>일정</p>
-            <input type="date"></input>
+            <input type="date" name='fromDate' onChange={onChangePost}></input>
             <span>~</span>
-            <input type="date"></input>
+            <input type="date" name='toDate' onChange={onChangePost}></input>
           </li>
           <li>
             <p>일기</p>
-            <textarea></textarea>
+            <textarea name='content' onChange={onChangePost}></textarea>
           </li>
         </ul>
         <div className="postWriteBtn">
-          <button type="submit">등록</button>
-          <button type="cancel">취소</button>
+          <span onClick={onSubmit}>등록</span>
+          <span onClick={goMain}>메인으로</span>
         </div>
       </form>
     </div>
