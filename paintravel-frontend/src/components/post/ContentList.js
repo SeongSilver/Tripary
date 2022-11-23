1eea37f59678afbbb5fd9235a5906ab1b25ac241
SoobeenMun <96978871+NYANGSoobeen@users.noreply.github.com>
2022-11-24 02:32:24 +0900

    contentList html 수정 / contentModal 기능 구현

--- paintravel-frontend/src/components/post/ContentList.js
+++ paintravel-frontend/src/components/post/ContentList.js
@@ -6,10 +6,12 @@ import '../../styles/post/contentList.scss';
 
 function ContentList({selectedCountry}) {
   const [contentModal, setContentModal] = useState(false);
+  const [contentModalStatus, setContentModalStatus] = useState(false);
   const [cityName, setCityName] = useState("서울인건가");
 
   const openContentModal = (event) => {
     setContentModal(true);
+    setContentModalStatus(true);
     //해당 게시물의 디테일 정보를 가져올 get 연동 할 것들
     //axios.get(어쩌고 저쩌고)
   }
@@ -29,55 +31,63 @@ function ContentList({selectedCountry}) {
         <li className="contentCard">
             <span className="cardTag">서울에서 얼마나 길어지는지 태에ddd에에그</span>
             <div>
-              {/* 수빈 - img 불러올 때 img 태그에 alt="자세히 보기" 적어주세요! */}
-              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} alt="자세히 보기"/>
+              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} />
             </div>
             <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
+            <Link to="#" onClick={openContentModal}>일기 자세히 보기</Link>
+        </li>
+        <li className="contentCard">
+            <span className="cardTag">광화문</span>
+            <div>
+              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} />
+            </div>
+            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
+            <Link to="#" onClick={openContentModal}>일기 자세히 보기</Link>
+        </li>
+        <li className="contentCard">
+            <span className="cardTag">여수</span>
+            <div>
+              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} />
+            </div>
+            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
+            <Link to="#" onClick={openContentModal}>일기 자세히 보기</Link>
+        </li>
+        <li className="contentCard">
+            <span className="cardTag">속초</span>
+            <div>
+              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} />
+            </div>
+            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
+            <Link to="#" onClick={openContentModal}>일기 자세히 보기</Link>
+        </li>
+        <li className="contentCard">
+            <span className="cardTag">제주도</span>
+            <div>
+              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} />
+            </div>
+            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
+            <Link to="#" onClick={openContentModal}>일기 자세히 보기</Link>
+        </li>
+        <li className="contentCard">
+            <span className="cardTag">부산</span>
+            <div>
+              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} />
+            </div>
+            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
+            <Link to="#" onClick={openContentModal}>일기 자세히 보기</Link>
         </li>
         <li className="contentCard">
             <span className="cardTag">서울</span>
             <div>
-              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} alt="자세히 보기"/>
+              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} />
             </div>
             <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
-        </li>
-        <li className="contentCard">
-            <span className="cardTag">서울</span>
-            <div>
-              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} alt="자세히 보기"/>
-            </div>
-            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
-        </li>
-        <li className="contentCard">
-            <span className="cardTag">서울</span>
-            <div>
-              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} alt="자세히 보기"/>
-            </div>
-            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
-        </li>
-        <li className="contentCard">
-            <span className="cardTag">서울</span>
-            <div>
-              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} alt="자세히 보기"/>
-            </div>
-            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
-        </li>
-        <li className="contentCard">
-            <span className="cardTag">서울</span>
-            <div>
-              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} alt="자세히 보기"/>
-            </div>
-            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
-        </li>
-        <li className="contentCard">
-            <span className="cardTag">서울</span>
-            <div>
-              <img className="contentImage" src={require("../../img/login/loginBg.jpg")} alt="자세히 보기"/>
-            </div>
-            <p className="cardDate">2022-01-01 ~ 2022-12-31</p>
+            <Link to="#" onClick={openContentModal}>일기 자세히 보기</Link>
         </li>
       </ul>
-      {contentModal && <ContentModal className="contentModal" setContentModal={setContentModal} cityName={cityName}/>}
+      {contentModal && <ContentModal className="contentModal" 
+      setContentModal={setContentModal} setContentModalStatus={setContentModalStatus} 
+      contentModalStatus={contentModalStatus} cityName={cityName}/>}
     </div>
   );
 }
