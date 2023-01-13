cea9e0bc9923495e17e8e09aa342a48432053677
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2023-01-13 22:50:56 +0900

    모달에 이미지 띄우기 전까지 작업

--- paintravel-frontend/src/components/post/ContentModal.js
+++ paintravel-frontend/src/components/post/ContentModal.js
@@ -7,19 +7,35 @@ function ContentModal({ data, setCheck }) {
     setCheck(false);
   };
   // 슬라이드 animation 이전 버튼
+  console.log(data.file);
 
   return (
     <div className="modalContainer">
       <div className="modalDiv">
-        제발 모달좀 나와줘라
-        {data.file.map((image, id) => {
-          <div key={id}>
-            <img src={`/upload/${image}`} alt={`${image} - ${id}`} />
-          </div>;
-        })}
+        {/* <div className="modalHeader"> */}
+        {/* <h1>{cityName}</h1> */}
+        <h2>{data.title}</h2>
+        {/* </div> */}
         {data.title}
         {data.location}
         {data.content}
+        <ul className="modalBody">
+          {data.file.map((image, index) => {
+            <li key={index}>
+              <img src={`/upload/${image}`} />
+            </li>;
+          })}
+        </ul>
+        <Link
+          to="/postEdit"
+          state={{
+            selectedCountry: data.country,
+            nationCode: data.nationCode,
+            _id: data._id,
+            writer: data.writer,
+          }}>
+          <button>수정</button>
+        </Link>
         <button onClick={closeModal}>닫아줘</button>
       </div>
     </div>
