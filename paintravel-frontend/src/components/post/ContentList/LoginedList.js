3a1bde67e45f271847936e9900128775042dcced
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2023-01-13 22:24:37 +0900

    일단 모달에 데이터 띄우기 닫기는 되는것으로 확인...

--- paintravel-frontend/src/components/post/ContentList/LoginedList.js
+++ paintravel-frontend/src/components/post/ContentList/LoginedList.js
@@ -3,41 +3,29 @@ import { Link } from "react-router-dom";
 import ContentModal from "../ContentModal";
 
 function LoginedList({ listData }) {
-  const [contentModal, setContentModal] = useState(false);
-  const [contentModalStatus, setContentModalStatus] = useState(false);
+  const [check, setCheck] = useState(false);
 
   const openContentModal = () => {
-    setContentModal(true);
+    setCheck(true);
   };
-  console.log(listData[0])
+  console.log(listData[0]);
   return (
     // <div className="contentBody">하하</div>
     <ul className="contentBody">
       {listData &&
-        listData.map(
-          (data) => (
-            <li className="contentCard" key={data._id}>
-              <span className="cardTag">{data.location}</span>
-              <Link
-                to="/postEdit"
-                state={{
-                  selectedCountry: data.country,
-                  nationCode: data.nationCode,
-                  _id: data._id,
-                  writer : data.writer
-              }}>
-                <div>
-                  <img className="contentImage" src={`/upload/${data.file[0]}`} />
-                </div>
-              </Link>
-              <p className="cardDate">
-                {new Date(data.fromDate).toLocaleDateString()} ~{" "}
-                {new Date(data.toDate).toLocaleDateString()}
-              </p>
-              {/* {contentModal && <ContentModal contentModalStatus={contentModalStatus} setContentModalStatus={setContentModalStatus} setContentModal={ setContentModal}  listData={data} />} */}
-            </li>
-          )
-          )}
+        listData.map((data) => (
+          <li className="contentCard" key={data._id}>
+            <span className="cardTag">{data.location}</span>
+            <div onClick={openContentModal}>
+              <img className="contentImage" src={`/upload/${data.file[0]}`} />
+            </div>
+            <p className="cardDate">
+              {new Date(data.fromDate).toLocaleDateString()} ~{" "}
+              {new Date(data.toDate).toLocaleDateString()}
+            </p>
+            {check && <ContentModal data={data} setCheck={setCheck} />}
+          </li>
+        ))}
     </ul>
   );
 }
