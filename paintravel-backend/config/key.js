780300d15a720e0607cb383ab2e5a5b1e5e1af6c
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-01-26 18:59:51 +0900

    main.yml 수정-2

--- paintravel-backend/config/key.js
+++ paintravel-backend/config/key.js
@@ -1,11 +1,11 @@
 //비밀 설정 정보 관리.
 //횐경변수 process.env.NODE_ENV가 production 이면 배포되었음으로 prod.js에서 키 받아오기
-if (process.env.NODE_ENV === "production") {
-  console.log(process.env.NODE_ENV)
+if (process.env.REACT_APP_NODE_ENV === "production") {
+  console.log(process.env.REACT_APP_NODE_ENV)
   console.log("배포배포")
   module.exports = require("./prod");
 } else {
-  console.log(process.env.NODE_ENV)
+  console.log(process.env.REACT_APP_NODE_ENV)
   console.log("데브데브")
   module.exports = require("./dev");
 }
