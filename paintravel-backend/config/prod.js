6d6d02bfb9cf0544762841c2e4950227f8acf8e4
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-01-26 19:18:17 +0900

    mongoURI 확인작업-2

--- paintravel-backend/config/prod.js
+++ paintravel-backend/config/prod.js
@@ -1,6 +1,6 @@
 //비밀 정보 관리(배포 환경)
-console.log(process.env.mongoURI)
+console.log(process.env.REACT_APP_MONGOURI)
 module.exports = {
   //mongo_URI라는 변수로 배포환경에 저장된 변수를 사용해라.
-  mongoURI: process.env.mongo_URI,
+  mongoURI: process.env.REACT_APP_MONGOURI,
 };
