4a451791244173b3c238271cc9350f0931197fe0
SoobeenMun <96978871+NYANGSoobeen@users.noreply.github.com>
2023-01-26 22:19:49 +0900

    반응형 작업 진행중
    - html 기본 폰트 사이즈 변경
     ( 기존 vw -> px )
    - login, signUp, contentList, contentModal 반응형 작업 및 퍼블리싱 수정

--- paintravel-frontend/src/components/post/ContentList/EmptyList.js
+++ paintravel-frontend/src/components/post/ContentList/EmptyList.js
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 import '../../../styles/post/contentList/emptyList.scss';
 
 function EmptyList({ selectedCountry, nationCode }) {
-  const [airplaneStyle, setAirplaneStyle] = useState({});
+  const [penStyle, setPenStyle] = useState({});
   useEffect (() => {
     document.querySelector(".emptyListContainer > a").addEventListener("mouseover", function(){
-      setAirplaneStyle({transform:"scale(2) translate(50%, -40%)"});
+      setPenStyle({animation:"penShake 1s infinite, penMoving 3s infinite"});
     });
     document.querySelector(".emptyListContainer > a").addEventListener("mouseleave", function(){
-      setAirplaneStyle({});
+      setPenStyle({});
     });
   });
   return (
     <div className="emptyListContainer">
       <figure>
         <img src={require("../../../img/post/diary.png")} alt="다이어리 이미지" />
-        <img className="airplane" style={airplaneStyle}
-        src={require("../../../img/post/airplane.png")} alt="비행기 이미지" />
+        <img className="airplane" style={penStyle}
+        src={require("../../../img/post/diaryPen.png")} alt="비행기 이미지" />
       </figure>
       <p>아직 여행 일지가 없어요 !</p>
       <Link
